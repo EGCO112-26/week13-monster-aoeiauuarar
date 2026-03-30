032f5e05e24d256f557e498bb5596aedf5245070
aoeiauuarar <tawangood46@gmail.com>
2026-03-30 08:36:50 +0000

    Test monster

--- main.cpp
+++ main.cpp
@@ -6,10 +6,23 @@
 //
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 #include "monster.h"
 
 int main(int argc, const char * argv[]) {
- 
+    int n = 10;
+    int h,pt;
+    string na;
+    monster* a[10];
+    for(int i = 0;i<n;i++){
+        cout << "Enter Monster " << i+1 << " (Name HP Potion): ";
+        cin>>na>>h>>pt;
+        a[i] = new monster(na, h, pt);
+    }
+    
+    for(int i = 0; i < n; i++){
+        delete a[i];
+    }
     return 0;
 }
--- monster.h
+++ monster.h
@@ -13,9 +13,21 @@ private:
     int hp,potion;
 public:
     void Attack(monster &);
-          void heal();
-    monster(string, int,int);
+    void heal();
+    monster(string="T", int=100,int=5);
+    ~monster();
 };
 
+monster::monster(string na, int h,int p){
+    name = na;
+    h>200?hp=200:hp=h;
+    p>10?potion=10:potion=p;
+
+    cout<<setw(10)<<"Monster : "<<name<<endl<<setw(10)<<"HP : "<<hp<<endl<<setw(10)<<"Potion : "<<potion<<endl;
+}
+
+monster::~monster(){
+    cout<<"Bye bye"<<" Monster : "<<name<<endl;
+}
 
 #endif /* monster_h */
